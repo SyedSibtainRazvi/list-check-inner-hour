e7f2dcd675f6568776eafd2c026460fe1652db4a
SyedSibtainRazvi <Syedsibtain191@gmail.com>
2022-06-20 16:34:00 +0530

    doc: Structed doc

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment }from 'react';
+import React, { Fragment } from 'react';
 import { Form, FormGroup, Label, Col, Input, Button } from 'reactstrap';
 
 
@@ -6,17 +6,18 @@ import { Form, FormGroup, Label, Col, Input, Button } from 'reactstrap';
 const App = () => {
   return (
     <Fragment>
+      <h1> List-Check </h1>
       <Form className='container mt-5'>
         <FormGroup row>
           <Label for="list1" sm={2}>
             List 1
           </Label>
           <Col sm={10}>
-          <Input 
-          value = ""
-          name = "list1"
-          id = "list1"
-          placeholder = "Enter values seperated by comma"/>
+            <Input
+              value=""
+              name="list1"
+              id="list1"
+              placeholder="Enter values seperated by comma" />
           </Col>
         </FormGroup>
 
@@ -25,14 +26,42 @@ const App = () => {
             List 2
           </Label>
           <Col sm={10}>
-          <Input 
-          value = ""
-          name = "list2"
-          id = "list2"
-          placeholder = "Enter values seperated by comma"/>
+            <Input
+              value=""
+              name="list2"
+              id="list2"
+              placeholder="Enter values seperated by comma" />
           </Col>
         </FormGroup>
       </Form>
+
+      <Button color='primary'>Compute
+
+      </Button>
+
+      <main>
+        <h1>Results</h1>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-sm'>
+              <h3> Items present only in list A</h3>
+            </div>
+            <div className='col-sm'>
+              <h3>Items present only in list B</h3>
+            </div>
+            <div className='row'>
+              <div className='col-sm'>
+                <h3> Items present in both A and B</h3>
+              </div>
+              <div className='col-sm'>
+                <h3>Items combining both A and B</h3>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </main>
     </Fragment>
   )
 }
